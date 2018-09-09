b27146052e8a5976f843e3d72d915672e133ff18
daominhtoan09 <daominhtoan2612@gmail.com>
2018-09-09 14:03:54 +0700

    next

---
+++ Front_end/Day_8/example.js
@@ -0,0 +1,9 @@
+function showResult() {
+    let a = document.getElementById("number1").innerText;
+    let b = document.getElementById("number2").innerText;
+
+    let c = parseInt(a) + parseInt(b);
+    document.getElementById('result').innerText = c;
+    let a = prompt("input ");
+    let a = prompt("input ");
+}
